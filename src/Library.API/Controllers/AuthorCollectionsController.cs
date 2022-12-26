f778e6ee63e19bcc8eecd54068fe45798d93644c
Khatab-mohamed <54792306+Khatab-mohamed@users.noreply.github.com>
2022-12-26 06:58:25 -0800

    Creating A Collection of Resources

--- src/Library.API/Controllers/AuthorCollectionsController.cs
+++ src/Library.API/Controllers/AuthorCollectionsController.cs
@@ -1,10 +1,12 @@
 using AutoMapper;
 using Library.API.Dtos;
 using Library.API.Entities;
+using Library.API.Helpers;
 using Library.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Library.API.Controllers
 {
@@ -17,22 +19,45 @@ namespace Library.API.Controllers
         {
             _libraryRepository = libraryRepository;
         }
+       
+        
         [HttpPost]
-        public IActionResult CreateAuthorCollection(IEnumerable<AuthorCreationDto> authorCollection)
+        public IActionResult CreateAuthorCollection([FromBody]IEnumerable<AuthorCreationDto> authorCollection)
         {
             if (authorCollection == null)
-                return BadRequest();
-             
+            { return BadRequest(); }
+
             var authorEntities = Mapper.Map<IEnumerable<Author>>(authorCollection);
-           
-            
+            var authors = authorEntities;
+
+
             foreach (var author in authorEntities)
             {
                 _libraryRepository.AddAuthor(author);
             }
             if (!_libraryRepository.Save())
+            {
                 throw new Exception("Creating an Author collection failed on save.");
-            return Ok();
+            }
+            
+            
+            var authorCollectionToReturn = Mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a=>a.Id));
+            
+            return CreatedAtRoute("GetAuthorCollection",new { ids= idsAsString},authorCollectionToReturn);
+        }
+
+        [HttpGet("{ids}",Name ="GetAuthorCollection")]
+        public IActionResult GetAuthorsCollection([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if(ids==null)
+                return BadRequest();
+           var authorsEntities =  _libraryRepository.GetAuthors(ids);
+            
+            if(ids.Count() != authorsEntities.Count())
+                return NotFound();
+            var authorsToReturn = Mapper.Map<IEnumerable<AuthorDto>>(authorsEntities);
+            return Ok(authorsToReturn);
         }
 
     }
